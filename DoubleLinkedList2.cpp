ac69146abf2fa1ee14a84c5cc53b835de871e2a7
rismamutia <rismamutiadewi89@gmail.com>
2025-05-15 16:11:52 +0700

    membuat fungsi untuk menghapus node

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -126,5 +126,9 @@ public:
             if(current->next != NULL)
                 current->next->prev = current->prev;
         }
+
+        // Step 5:Delete the node
+        delete current;
+        cout << "Record with roll number" << rollNo << "deleted" << endl;
     }
 };
