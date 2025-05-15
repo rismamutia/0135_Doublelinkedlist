2cca210157ea190105fe01b2c16ec8119e9f824e
rismamutia <rismamutiadewi89@gmail.com>
2025-05-15 15:42:10 +0700

    membuat fungsi new node

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -55,6 +55,21 @@ public:
             START= newNode;
             return;
         }
+        // Insert in between mode
+        // Step 8: Locate position for insertion
+        Node *current = START;
+        while(current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+        // Step 9: Insert between current and current->next
+        newNode->next = current->next; // Step 9a: newNode.next = current.next
+        newNode->prev = current;       // Step 9b: newNode.prev = current
         
     }
 }
