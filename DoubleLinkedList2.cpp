ca7ea23e73b1faf7e215be3d44bb5322297769cc
rismamutia <rismamutiadewi89@gmail.com>
2025-05-15 15:45:31 +0700

    membuat fungsi menambah data pada akhir node

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -70,6 +70,10 @@ public:
         // Step 9: Insert between current and current->next
         newNode->next = current->next; // Step 9a: newNode.next = current.next
         newNode->prev = current;       // Step 9b: newNode.prev = current
-        
+
+        // insert last node
+        if(current->next != NULL)
+        current->next->prev = newNode;  //Step 9c: current.next.prev = newNode
+        current->next = newNode;  //Step 9d: current.next = newNode
     }
 }
